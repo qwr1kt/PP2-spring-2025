7e0858ba0b88d2b02da9988df904371d6eb31178
qwr1kt <musinaliaskar@gmail.com>
2025-02-18 19:34:31 +0500

    json

--- lab4/json/task1.py
+++ lab4/json/task1.py
@@ -1,16 +1,27 @@
 import json 
 
-print("Interface status")
-print("================================================================================")
-print("DN                                                 Description           Speed    MTU  ")
-print("-------------------------------------------------- --------------------  ------  ------")
+with open("/Users/aliaskarmussin/Desktop/PP2-spring-2025/lab4/json/sample-data.json", "r") as file:
 
-with open("/Users/aliaskarmussin/Desktop/PP2-spring-2025/lab4/json/sample-data.json") as f:
-    data = json.load(f)
+    data = json.load(file)
 
-request_from_server = data["imdata"][0]["l1PhysIf"]["attributes"]["dn"]
-speed = data["imdata"][0]["l1PhysIf"]["attributes"]["fecMode"]
-mtu = data["imdata"][0]["l1PhysIf"]["attributes"]["mtu"]
-print(request_from_server, "                            ", speed," ", mtu )
-print(request_from_server, "                            ", speed," ", mtu )
-print(request_from_server, "                            ", speed," ", mtu )
+print("Interface Status")
+
+print("=" * 80)
+
+print(f"{'DN':<50} {'Description':<20} {'Speed':<8} {'MTU':<6}")
+
+print("-" * 80)
+
+for item in data["imdata"]:
+
+    attributes = item["l1PhysIf"]["attributes"]
+
+    dn = attributes["dn"]
+
+    description = attributes["descr"] if attributes["descr"] else ""
+
+    speed = attributes["speed"]
+
+    mtu = attributes["mtu"]
+
+    print(f"{dn:<50} {description:<20} {speed:<8} {mtu:<6}")
