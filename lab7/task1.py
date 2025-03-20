06e91dad147c780a35bbac49a819127640b9e5ca
qwr1kt <musinaliaskar@gmail.com>
2025-03-21 02:31:34 +0500

    lab7

--- lab7/task1.py
+++ lab7/task1.py
@@ -8,8 +8,8 @@ clock = pygame.time.Clock()
 pygame.display.set_caption("Mickey Clock")
 
 leftarm = pygame.image.load("/Users/aliaskarmussin/Desktop/PP2-spring-2025/lab7/images/leftarm.png")  # Левая рука - сек
-rightarm = pygame.image.load("/Users/aliaskarmussin/Desktop/PP2-spring-2025/lab7/images/rightarm.png")  # Правая рука (минутная стрелка)
-mainclock = pygame.transform.scale(pygame.image.load("/Users/aliaskarmussin/Desktop/PP2-spring-2025/lab7/images/clock.png"), (800, 600))  # Фон часов
+rightarm = pygame.image.load("/Users/aliaskarmussin/Desktop/PP2-spring-2025/lab7/images/rightarm.png")  # Правая рука - мин
+mainclock = pygame.transform.scale(pygame.image.load("/Users/aliaskarmussin/Desktop/PP2-spring-2025/lab7/images/clock.png"), (800, 600)) 
 
 done = False
 
@@ -30,27 +30,22 @@ while not done:
     minute_shift = 8  # Смещение на 10 минут вперед
     minute_angle = -(minute + minute_shift) * 6 - (second / 60) * 6  # 360° / 60 минут = 6° за каждую минуту
 
-    # Отображаем фон (изображение часов)
     screen.blit(mainclock, (0, 0))
 
-    # Отрисовка минутной стрелки (правая рука)
     rotated_rightarm = pygame.transform.rotate(
         pygame.transform.scale(rightarm, (800, 600)), minute_angle
     )
     rightarm_rect = rotated_rightarm.get_rect(center=(400, 300))
     screen.blit(rotated_rightarm, rightarm_rect)
 
-    # Отрисовка секундной стрелки (левая рука)
     rotated_leftarm = pygame.transform.rotate(
         pygame.transform.scale(leftarm, (40, 600)), second_angle
     )
     leftarm_rect = rotated_leftarm.get_rect(center=(400, 300))
     screen.blit(rotated_leftarm, leftarm_rect)
 
-    # Обновляем экран
     pygame.display.flip()
 
-    # Ограничиваем FPS до 60 кадров в секунду
     clock.tick(60)
 
 pygame.quit()
